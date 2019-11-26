fef4260c07c43833d9e85043c42f0fab5af6a403
21006 <21006@MININT-VTTS8LK.columbushs.com>
2019-11-26 08:49:31 -0500

    Done

--- src/atm/machine/ATMMachine.java
+++ src/atm/machine/ATMMachine.java
@@ -1,21 +1,69 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package atm.machine;
-
+import java.util.Scanner;
 /**
  *
  * @author mlarrubia
  */
 public class ATMMachine {
+    static Scanner kb = new Scanner(System.in);
+    private static final String USERNAME = "user";
+    
+    private static final int PIN = 1234;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("Login: ");
+        System.out.println("Username > ");
+        String name = kb.nextLine();
+        System.out.println("Pin > ");
+        int pinCode = kb.nextInt();
+        
+        if(name.equals(USERNAME) && PIN == pinCode){
+           
+           accountManagement();
+            
+           
+           
+        }
+        else{
+            System.out.println("Try again");
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
         // TODO code application logic here
     }
+    
+    public static void accountManagement(){
+        System.out.println("WELCOME, user!");
+        System.out.println("1. Check Account Balance");
+        System.out.println("2. Deposit Checks");
+        System.out.println("3. Withdraw Money");
+        System.out.println("4. Logout");
+        
+        
+        
+        
+    }
+    
+    
+    
+    
     
 }
